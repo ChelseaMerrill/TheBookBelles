cdbc16c121b7d8d9636f99913e9fab5efd55905a
Chelsea Merrill <chelmer1223@aol.com>
2021-05-27 19:04:13 -0400

    added icon

--- src/Components/Homepage/homepage.components.jsx
+++ src/Components/Homepage/homepage.components.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import {Redirect} from 'react-router-dom';
+import Icon from './icon_gray.jpg';
 
 
 
@@ -8,6 +9,7 @@ class Homepage extends React.Component{
     render () {
         return (
             <div className='home'>
+                <img src={Icon}/>
                 <p>this is the homepage</p>
             </div>
            
