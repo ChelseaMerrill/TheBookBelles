488e605c1c9d2f63bc81970c60a03f22f126bbab
Chelsea Merrill <chelmer1223@aol.com>
2021-06-02 08:34:15 -0400

    more notes

--- src/notes.js
+++ src/notes.js
@@ -1,5 +1,6 @@
 //Questions:
     //can I make a card a set size?
+    //how do i map so I am displaying two ID's in one object div, and the rest in the other? --> meet page
 
 
 //REC Page
@@ -11,3 +12,5 @@
 //GIVE REC page
     //connect to my gmail
     //set up input areas for; title, author, rating, review, by
+
+
