7f92365b7ebaf7bb8c8c730c953fea8b961bb297
Chelsea Merrill <chelmer1223@aol.com>
2024-04-09 17:02:25 -0400

    wiped repo and starting fresh

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
-import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Homepage from "./Components/Homepage/homepage";
-import Navigation from './Components/NavBar/navbar.components';
-import MeetTheBelles from "./Components/Meet_The_Belles/meet";
-import BookOfTheMonth from "./Components/Book_Of_The_Month/bookofthemonth";
-import Recommendation from './Components/Recommendations/recommendations.components';
-import GiveARec from "./Components/Give_A_Recommendation/givearec";
-import Jumbotron from "./Components/Jumbotron/jumbotron";
 
 
 import './App.css';
@@ -14,20 +8,10 @@ function App() {
   return (
     <Router>
       <div>
-        <div>
-          <Jumbotron/>
-          <Navigation/>
-        </div>
         <Switch>
           <Route exact path="/" component={Homepage} />
-          <Route exact path="/meet" component={MeetTheBelles} />
-          <Route exact path="/bookofthemonth" component={BookOfTheMonth} />
-          <Route exact path="/recommendation" component={Recommendation} />
-          <Route exact path="/givearec" component={GiveARec} />
         </Switch>
-
       </div>
-          {/* <Footer/> */}
     </Router>
   );
 }
--- src/Components/Homepage/homepage.js
+++ src/Components/Homepage/homepage.js
@@ -1,8 +1,5 @@
-import React, {Component} from 'react';
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
-import {Redirect} from 'react-router-dom';
-import Icon from './icon.jpg';
-import BOTMI from './when_no_one_is_watching.jpg';
+import React from 'react';
+
 
 import './homepage.css';
 
@@ -13,21 +10,6 @@ class Homepage extends React.Component{
                 <h1>The Book Belles</h1>
                 <div className='bio'>
                     <h2>The Book Belles book club was created in the midst of the COVID pandemic when two friends decided to take their love of literature and use it to bridge the distance between friends. Each month we explore new authors, genres, and time periods to diversify the experience and discover something new. What began as a childhood dream of two girls in Upsate, New York has rapidly expanded to encompass women in different states and, hopefully, eventually globally.  We meet virtually on the last Sunday of the month to share thoughts, explore plot lines, and celebrate friendship. We are always welcoming new members who want to be a part of our little community. We hope that you join us! </h2>
-                </div>
-                     <h1>What We Are Reading</h1>
-                     <h1>June 2021</h1>
-                 <div className='bookOfTheMonth'>
-                    <div className='content'>
-                        <img src={BOTMI} width='300px' height='410px'/>
-                        <h2>Title: When No One Is Watching</h2>
-                        <h3>Author: Alyssa Cole</h3>
-                    </div>
-                    <div className='summary'>
-                        <p>Sydney Green is Brooklyn born and raised, but her beloved neighborhood seems to change every time she blinks. Condos are sprouting like weeds, FOR SALE signs are popping up overnight, and the neighbors she’s known all her life are disappearing. To hold onto her community’s past and present, Sydney channels her frustration into a walking tour and finds an unlikely and unwanted assistant in one of the new arrivals to the block—her neighbor Theo. But Sydney and Theo’s deep dive into history quickly becomes a dizzying descent into paranoia and fear. Their neighbors may not have moved to the suburbs after all, and the push to revitalize the community may be more deadly than advertised. When does coincidence become conspiracy? Where do people go when gentrification pushes them out? Can Sydney and Theo trust each other—or themselves—long enough to find out before they too disappear?</p>
-                    </div>
-                </div>
-                <div className='end'>
-                    <h1>Meetings are held virtually the last Sunday of the month at 2 pm EST.  Email Chelsea at: chelmer1223@aol.com to join! </h1>
                 </div>
             </div>
            
